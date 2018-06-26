24941b16ad1e15deed7b6c22de5a3416c548f9cb
ananjarusarunchai <anan.answer@gmail.com>
2018-06-27 00:36:01 +0700

    initialize app

---
+++ src/components/course/CoursePage.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+export default class CoursePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Course</h1>
+      </div>
+    )
+  }
+}
